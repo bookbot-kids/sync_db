a7a2937997af79b9dca5d5b20c67cf077516971a
Adrian DeWitts <adrian.dewitts@gmail.com>
2019-11-24 19:24:11 +1100

    Setup tests

--- lib/src/robust_http.dart
+++ lib/src/robust_http.dart
@@ -2,19 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:intl/intl.dart';
 
-/// await HTTP.get('/path').then((Response response) {
-/// 
-/// }, onError: (error) {
-/// 
-/// });
-
-abstract class Config {
-  static String baseUrl;
-  static int connectTimeout;
-  static int receiveTimeout;
-  static int httpRetries;
-}
-
 class UnknownException implements Exception {
   String devDescription;
   UnknownException(this.devDescription);
@@ -34,33 +21,30 @@ class UnexpectedResponseException implements Exception {
 }
 
 class HTTP {
-  // Get http default options from the Config class
-  static BaseOptions options = new BaseOptions(
-    baseUrl: Config.baseUrl,
-    connectTimeout: Config.connectTimeout,
-    receiveTimeout: Config.receiveTimeout,
-  );
-  static Dio dio = new Dio(options);
+  static String baseUrl = 'https://www.google.com';
+  static int connectTimeout = 10000;
+  static int receiveTimeout = 10000;
+  static int httpRetries = 3;
 
-  /// Will do a http GET (with optional Dio BaseOptions overrides).
+  /// Does a http GET (with optional Dio BaseOptions overrides).
   /// You can pass the full url, or the path after the baseUrl.
   /// Will timeout, check connecctivity and retry until there is a response
   static Future<Response> get(String url, [BaseOptions options]) async {
-    // Normalise the url
-    if (!(url.startsWith('http://') || url.startsWith('https://'))) {
-      String baseUrl = options.baseUrl ?? HTTP.options.baseUrl;
-      url = baseUrl + url;
-    }
+    // Use Dio with properties from function or class
+    options ??= options = new BaseOptions(
+      baseUrl: HTTP.baseUrl,
+      connectTimeout: HTTP.connectTimeout,
+      receiveTimeout: HTTP.receiveTimeout,
+    );
 
-    // Use the Dio with properties from Config or BaseOptions that are passed in
-    Dio localDio = (options != null) ? new Dio(options) : HTTP.dio;
+    Dio dio = new Dio(options);
 
     // Make call, and manage the many network problems that can happen.
     // Will only throw an exception when it's sure that there is no internet connection, exhausts its retries or gets an unexpected server response
-    for (var i = 1; i <= Config.httpRetries ?? 3; i++) {
+    for (var i = 1; i <= HTTP.httpRetries; i++) {
       try {
-        return await localDio.get(url);
-      } on DioError catch(error) {
+        return await dio.get(url);
+      } catch(error) {
         if (error.type == DioErrorType.CONNECT_TIMEOUT || error.type == DioErrorType.RECEIVE_TIMEOUT) {
           if (await Connectivity().checkConnectivity() == ConnectivityResult.none) {
             throw ConnectivityException();
@@ -74,8 +58,5 @@ class HTTP {
     }
     // Exhausted retries, so send back exception
     throw RetryFailureException();
-
-    // if error in response code - return with error - UNEXPECTED RESPONSE error type
-    // Other error - return with error, pass error
   }
 }
--- test/sync_db_test.dart
+++ test/sync_db_test.dart
@@ -1,16 +1,29 @@
 import 'package:sync_db/sync_db.dart';
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
-
+  group('HTTP: ', () {
     setUp(() {
-      awesome = Awesome();
+      HTTP.baseUrl = 'https://httpstat.us/';
+      HTTP.connectTimeout = 3000;
+      HTTP.receiveTimeout = 3000;
+    });
+  
+    test('Test full url', () async {
+      expect((await HTTP.get('https://httpstat.us/200')).statusCode, equals(200));
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('Test path', () async {
+      expect((await HTTP.get('200')).statusCode, equals(200));
+    });
+
+    test('Test bad response gets exception', () async {
+      expect(HTTP.get('501'), throwsException);
+    });
+
+    test('Test timeout', () async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      expect(HTTP.get('https://httpstat.us/200?sleep=5000'), throwsException);
     });
   });
 }
