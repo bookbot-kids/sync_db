85c341150eff43006af059a206b513741dd584b7
duc <duc@appiphany.com.au>
2020-01-22 16:08:20 +0700

    add body data

--- lib/src/robust_http.dart
+++ lib/src/robust_http.dart
@@ -34,28 +34,31 @@ class HTTP {
   /// You can pass the full url, or the path after the baseUrl.
   /// Will timeout, check connectivity and retry until there is a response.
   /// Will handle most success or failure cases and will respond with either data or exception.
-  Future<dynamic> post(String url, {Map<String, dynamic> parameters}) async {
-    return request("POST", url, parameters: parameters);
+  Future<dynamic> post(String url,
+      {Map<String, dynamic> parameters, dynamic data}) async {
+    return request("POST", url, parameters: parameters, data: data);
   }
 
   /// Does a http PUT (with optional overrides).
   /// You can pass the full url, or the path after the baseUrl.
   /// Will timeout, check connectivity and retry until there is a response.
   /// Will handle most success or failure cases and will respond with either data or exception.
-  Future<dynamic> put(String url, {Map<String, dynamic> parameters}) async {
-    return request("PUT", url, parameters: parameters);
+  Future<dynamic> put(String url,
+      {Map<String, dynamic> parameters, dynamic data}) async {
+    return request("PUT", url, parameters: parameters, data: data);
   }
 
   /// Make call, and manage the many network problems that can happen.
   /// Will only throw an exception when it's sure that there is no internet connection,
   /// exhausts its retries or gets an unexpected server response
   Future<dynamic> request(String method, String url,
-      {Map<String, dynamic> parameters}) async {
+      {Map<String, dynamic> parameters, dynamic data}) async {
     dio.options.method = method;
 
     for (var i = 1; i <= (httpRetries ?? this.httpRetries); i++) {
       try {
-        return (await dio.request(url, queryParameters: parameters)).data;
+        return (await dio.request(url, queryParameters: parameters, data: data))
+            .data;
       } catch (error) {
         await _handleException(error);
       }
