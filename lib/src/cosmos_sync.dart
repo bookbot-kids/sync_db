a79e244b8613d626d3286578bdf632a20cb0152f
duc <duc@appiphany.com.au>
2020-02-10 17:49:30 +0700

    Add cosmos http api

--- lib/src/cosmos_sync.dart
+++ lib/src/cosmos_sync.dart
@@ -1,13 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
 import "abstract.dart";
 import "query.dart";
 import "robust_http.dart";
+import 'package:crypto/crypto.dart';
 
 class CosmosSync extends Sync {
   static CosmosSync shared;
   HTTP http;
   Database _database;
-  User _user;
-  
+  User user;
+  static const String _apiVersion = "2018-12-31";
+  String databaseId;
+  String masterKey;
+
   Map<String, DateTime> _tableReadLock = {};
   Map<String, DateTime> _tableWriteLock = {};
 
@@ -15,14 +22,18 @@ class CosmosSync extends Sync {
   /// This will require the `databaseAccount` name, and database id `dbId` in the config map
   static Future<void> config(Map config) {
     shared = CosmosSync();
-    shared.http = HTTP('https://${config["databaseAccount"]}.documents.azure.com/dbs/${config["dbId"]}/');
+    shared.http = HTTP(
+        'https://${config["databaseAccount"]}.documents.azure.com/dbs/${config["dbId"]}/',
+        {"connectTimeout": 60000, "receiveTimeout": 60000});
+    shared.databaseId = config["dbId"];
+    shared.masterKey = config["dbMasterKey"];
   }
 
   /// SyncAll will run the sync across the complete database.
   /// Cosmos has a resource token structure so it knows which tables have read or write sync.
   /// Reading and writing of tables is done sequentially to manage load to the server.
   Future<void> syncAll() async {
-    final resourceTokens = await _user.resourceTokens();
+    final resourceTokens = await user.resourceTokens();
     final keys = resourceTokens.keys;
 
     // Loop through tables to read sync
@@ -41,7 +52,8 @@ class CosmosSync extends Sync {
   /// Read sync this table if it is not locked.
   Future<void> syncRead(String table) async {
     // Check if table is locked and return if it is
-    if (_tableWriteLock[table] != null && _tableWriteLock[table].isAfter(DateTime.now())) {
+    if (_tableWriteLock[table] != null &&
+        _tableWriteLock[table].isAfter(DateTime.now())) {
       return;
     }
 
@@ -50,16 +62,14 @@ class CosmosSync extends Sync {
 
     // Get the last record change timestamp on server side
     final query = Query().order("_ts desc").limit(1);
-    final record = _database.query(query)[0];
+    final record = _database.query(table, query)[0];
     String select;
     if (record == null || (record != null && record["_ts"] == null)) {
       select = "SELECT * FROM $table";
-    }
-    else {
+    } else {
       select = "SELECT * FROM $table WHERE _ts > ${record["_ts"]}";
     }
     final parameters = {"query": select};
-
 
     // TODO:
     // Get updated records from last _ts timestamp as a map
@@ -70,11 +80,12 @@ class CosmosSync extends Sync {
   /// Write sync this table if it has permission and is not locked.
   Future<void> syncWrite(String table) async {
     // Check if table is locked and return if it is
-    if (_tableReadLock[table] != null && _tableReadLock[table].isAfter(DateTime.now())) {
+    if (_tableReadLock[table] != null &&
+        _tableReadLock[table].isAfter(DateTime.now())) {
       return;
     }
     // Check if we have write permission on table
-    final resourceTokens = await _user.resourceTokens();
+    final resourceTokens = await user.resourceTokens();
     if (resourceTokens[table]["permissionMode"] != "All") {
       return;
     }
@@ -84,23 +95,82 @@ class CosmosSync extends Sync {
 
     // Get created records and save to Cosmos DB
     var query = Query().where({"_status": "createdAt"}).order("createdAt asc");
-    var records = _database.query<Map>(query);
+    var records = _database.query<Map>(table, query);
 
-    for (final record in records) {
-
-    }
+    for (final record in records) {}
 
     // Get records that have been updated and update Cosmos
     query = Query().where({"_status": "updatedAt"}).order("updatedAt asc");
-    records = _database.query<Map>(query);
+    records = _database.query<Map>(table, query);
 
-    for (final record in records) {
-
-    }
+    for (final record in records) {}
 
     // TODO:
     // Get record from Cosmos (if updated) and compare record to see which one is newer (newer _ts or updated_at)
     // Save record to Cosmos
     // (for Adrian) do another check to see if there are any local updated records after this to upload
+  }
+
+  String _getAuthorizationToken(String verb, String resourceType,
+      String resourceId, String date, String masterKey) {
+    List<int> base64Key = base64.decode(masterKey);
+    var hmacSha256 = new Hmac(sha256, base64Key);
+    var payLoad = verb.toLowerCase() +
+        "\n" +
+        resourceType.toLowerCase() +
+        "\n" +
+        resourceId +
+        "\n" +
+        date.toLowerCase() +
+        "\n" +
+        "" +
+        "\n";
+    var hashPayLoad = hmacSha256.convert(utf8.encode(payLoad)).bytes;
+    var signature = base64.encode(hashPayLoad);
+    return Uri.encodeComponent("type=master&ver=1.0&sig=$signature");
+  }
+
+  Future<void> _createDocument(
+      String table, String partitionKey, Map<String, dynamic> json) async {
+    var now = new DateTime.now().toUtc();
+    var httpDate = HttpDate.format(now);
+    var key = _getAuthorizationToken(
+        "post", "docs", "dbs/${databaseId}/colls/$table", httpDate, masterKey);
+    try {
+      http.headers = {
+        "Authorization": key,
+        "Content-Type": "application/json",
+        "x-ms-date": httpDate,
+        "x-ms-version": _apiVersion,
+        "x-ms-documentdb-partitionkey": "[\"$partitionKey\"]"
+      };
+      var response =
+          await http.post("/dbs/${databaseId}/colls/$table/docs", data: json);
+      print(response);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _updateDocument(String table, String id, String partitionKey,
+      Map<String, dynamic> json) async {
+    var now = new DateTime.now().toUtc();
+    var httpDate = HttpDate.format(now);
+    var key = _getAuthorizationToken("put", "docs",
+        "dbs/$databaseId/colls/$table/docs/$id", httpDate, masterKey);
+    try {
+      http.headers = {
+        "Authorization": key,
+        "Content-Type": "application/json",
+        "x-ms-date": httpDate,
+        "x-ms-version": _apiVersion,
+        "x-ms-documentdb-partitionkey": "[\"$partitionKey\"]"
+      };
+      var response =
+          await http.put("/dbs/$databaseId/colls/$table/docs/$id", data: json);
+      print(response);
+    } catch (e) {
+      print(e);
+    }
   }
 }
