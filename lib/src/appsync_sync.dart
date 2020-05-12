272e104cc489faa56aa98095a604b4469ea4302a
duc <duc@appiphany.com.au>
2020-05-12 15:43:49 +0700

    Add thread pool for sync task
    Add documentation

--- lib/src/appsync_sync.dart
+++ lib/src/appsync_sync.dart
@@ -6,9 +6,16 @@ import '../sync_db.dart';
 import "abstract.dart";
 import 'query.dart' as q;
 
+/**
+ * Aws AppSync client
+ */
 class AppSync extends Sync {
   static AppSync shared;
+  /** Thread pool for sync all **/
   final _pool = pool.Pool(1);
+
+  /** Thread pool for sync one **/
+  final _modelPool = pool.Pool(1);
   HttpLink _httpLink;
   BaseUser user;
   Database database;
@@ -28,109 +35,115 @@ class AppSync extends Sync {
 
   @override
   Future<void> syncAll() async {
-    // Get graph client base on token
-    await _getGraphClient();
+    _pool.withResource(() async {
+      try {
+        // Get graph client base on token
+        await _getGraphClient();
 
-    // query all schema
-    await _getSchema();
+        // query all schema
+        await _getSchema();
 
-    if (schema == null) {
-      throw SyncException('Can not get schema');
-    }
+        if (schema == null) {
+          throw SyncException('Can not get schema');
+        }
 
-    // Loop through tables to read sync
-    for (var model in _models) {
-      var table = model.tableName();
-      if (schema.containsKey(table)) {
-        await syncRead(table, graphClient);
-      } else {
-        printLog('table $table does not exist in schema', logLevel);
+        // Loop through tables to read sync
+        for (var model in _models) {
+          var table = model.tableName();
+          if (schema.containsKey(table)) {
+            await syncRead(table, graphClient);
+          } else {
+            printLog('table $table does not exist in schema', logLevel);
+          }
+        }
+
+        // Loop through tables to write sync
+        for (var model in _models) {
+          var table = model.tableName();
+          if (schema.containsKey(table)) {
+            await syncWrite(table, graphClient);
+          } else {
+            printLog('table $table does not exist in schema', logLevel);
+          }
+        }
+
+        printLog('Sync completed', logLevel);
+      } catch (err) {
+        printLog('Sync error: $err', logLevel);
       }
-    }
-
-    // Loop through tables to write sync
-    for (var model in _models) {
-      var table = model.tableName();
-      if (schema.containsKey(table)) {
-        await syncWrite(table, graphClient);
-      } else {
-        printLog('table $table does not exist in schema', logLevel);
-      }
-    }
+    });
   }
 
   @override
   Future<void> syncWriteOne(
       String table, Map<String, dynamic> localRecord, bool isCreated,
       [bool refresh]) async {
-    await _getGraphClient();
-    if (schema == null) {
-      await _getSchema();
-    }
+    _modelPool.withResource(() async {
+      try {
+        await _getGraphClient();
+        if (schema == null) {
+          await _getSchema();
+        }
 
-    if (schema == null) {
-      throw SyncException('Can not get schema');
-    }
+        if (schema == null) {
+          throw SyncException('Can not get schema');
+        }
 
-    if (isCreated) {
-      var fields = _getFields(table).join('\n');
-      fields += "\n _ts \n id";
-      var response = await _createDocument(table, fields, localRecord);
+        if (isCreated) {
+          var fields = _getFields(table);
+          var response = await _createDocument(table, fields, localRecord);
 
-      // update to local & set synced status after syncing
-      if (response != null) {
-        var newRecord = response['create${table}'];
-        await database.saveMap(table, newRecord['id'], newRecord,
-            status: 'synced', updatedAt: newRecord['_ts'] * 1000);
-      }
-    } else {
-      // sync read
-      var fields = _getFields(table).join('\n');
-      fields += "\n _ts \n id";
-      var query = """
-         query get$table {
-            get$table(id:"${localRecord['id']}") {
-            $fields
+          // update to local & set synced status after syncing
+          if (response != null) {
+            var newRecord = response['create${table}'];
+            await database.saveMap(table, newRecord['id'], newRecord,
+                status: 'synced', updatedAt: newRecord['_ts'] * 1000);
+          }
+        } else {
+          // sync read
+          var fields = _getFields(table);
+          var response = await _getDocument(table, fields, localRecord['id']);
+          printLog(response, logLevel);
+          dynamic remoteRecord;
+          if (response != null) {
+            remoteRecord = response['get$table'];
+            // update from appsync to local, set status to synced to prevent sync again
+            var localDate = localRecord['updatedAt'] / 1000;
+            if (localDate < remoteRecord['_ts']) {
+              await database.saveMap(table, remoteRecord['id'], remoteRecord,
+                  updatedAt: remoteRecord['_ts'] * 1000, status: 'synced');
             }
           }
-      """;
 
-      var response = await _queryDocuments(graphClient, query);
-      printLog(response, logLevel);
-      dynamic remoteRecord;
-      if (response != null) {
-        remoteRecord = response['get$table'];
-        // update from appsync to local, set status to synced to prevent sync again
-        var localDate = localRecord['updatedAt'] / 1000;
-        if (localDate < remoteRecord['_ts']) {
-          await database.saveMap(table, remoteRecord['id'], remoteRecord,
-              updatedAt: remoteRecord['_ts'] * 1000, status: 'synced');
-        }
-      }
+          // sync write
+          if (remoteRecord != null) {
+            if (remoteRecord['id'] == localRecord['id']) {
+              var localDate = localRecord['updatedAt'] / 1000;
+              // if local is newest, merge and save to appsync
+              if (localDate > remoteRecord['_ts']) {
+                localRecord.forEach((key, value) {
+                  if (key != 'updatedAt') {
+                    remoteRecord[key] = value;
+                  }
+                });
 
-      // sync write
-      if (remoteRecord != null) {
-        if (remoteRecord['id'] == localRecord['id']) {
-          var localDate = localRecord['updatedAt'] / 1000;
-          // if local is newest, merge and save to appsync
-          if (localDate > remoteRecord['_ts']) {
-            localRecord.forEach((key, value) {
-              if (key != 'updatedAt') {
-                remoteRecord[key] = value;
+                var response =
+                    await _updateDocument(table, fields, remoteRecord);
+                // update to local & set synced status after syncing
+                if (response != null) {
+                  var updatedRecord = response['update${table}'];
+                  await database.saveMap(
+                      table, updatedRecord['id'], updatedRecord,
+                      status: 'synced', updatedAt: updatedRecord['_ts'] * 1000);
+                }
               }
-            });
-
-            var response = await _updateDocument(table, fields, remoteRecord);
-            // update to local & set synced status after syncing
-            if (response != null) {
-              var updatedRecord = response['update${table}'];
-              await database.saveMap(table, updatedRecord['id'], updatedRecord,
-                  status: 'synced', updatedAt: updatedRecord['_ts'] * 1000);
             }
           }
         }
+      } catch (err) {
+        printLog('Sync error: $err', logLevel);
       }
-    }
+    });
   }
 
   @override
@@ -140,8 +153,7 @@ class AppSync extends Sync {
     var records = await database.query(query);
     final record = records.isNotEmpty ? records[0] : null;
     String select;
-    var fields = _getFields(table).join('\n');
-    fields += '\n _ts\n id';
+    var fields = _getFields(table);
 
     int limit = 10000;
     if (record == null || (record != null && record["_ts"] == null)) {
@@ -204,8 +216,7 @@ class AppSync extends Sync {
     var records = await database.query<Map>(query);
 
     for (final record in records) {
-      var fields = _getFields(table).join('\n');
-      fields += '\n _ts\n id';
+      var fields = _getFields(table);
 
       var response = await _createDocument(table, fields, record);
       // update to local & set synced status after syncing
@@ -222,17 +233,9 @@ class AppSync extends Sync {
 
     for (final localRecord in records) {
       // get appsync record
-      var fields = _getFields(table).join('\n');
-      fields += '\nid \n_ts';
-      var query = """
-         query get$table {
-            get$table(id:"${localRecord['id']}") {
-            $fields
-            }
-          }
-      """;
+      var fields = _getFields(table);
+      var response = await _getDocument(table, fields, localRecord['id']);
 
-      var response = await _queryDocuments(graphClient, query);
       printLog(response, logLevel);
       if (response != null) {
         var remoteRecord = response['get$table'];
@@ -260,11 +263,33 @@ class AppSync extends Sync {
     }
   }
 
+  /**
+   * Exclude fields from the local record before sending to server
+   */
   void _excludeLocalFields(Map map) {
     map.removeWhere((key, value) =>
         key == 'updatedAt' || key == 'createdAt' || key.startsWith('_'));
   }
 
+  /**
+   * Get document by id
+   */
+  Future<dynamic> _getDocument(String table, String fields, String id) async {
+    var query = """
+          query get$table {
+              get$table(id:"${id}") {
+              $fields
+              }
+            }
+          """;
+
+    return await _queryDocuments(graphClient, query);
+  }
+
+  /**
+   * Create new document
+   * Return a new document
+   */
   Future<dynamic> _createDocument(
       String table, String fields, Map record) async {
     _excludeLocalFields(record);
@@ -281,6 +306,10 @@ class AppSync extends Sync {
     return _mutationDocument(graphClient, query, variables);
   }
 
+  /**
+   * Update a document
+   * Return an updated document
+   */
   Future<dynamic> _updateDocument(
       String table, String fields, Map record) async {
     _excludeLocalFields(record);
@@ -297,6 +326,10 @@ class AppSync extends Sync {
     return _mutationDocument(graphClient, query, variables);
   }
 
+  /**
+   * Execute mutation query like update, insert, delete
+   * Return a document
+   */
   Future<dynamic> _mutationDocument(GraphQLClient graphClient, String query,
       Map<String, dynamic> variables) async {
     var options =
@@ -306,6 +339,10 @@ class AppSync extends Sync {
     return result.data;
   }
 
+  /**
+   * Query documents
+   * Return a list of document
+   */
   Future<dynamic> _queryDocuments(GraphQLClient graphClient, String query,
       [Map<String, dynamic> variables]) async {
     var options = QueryOptions(documentNode: gql(query), variables: variables);
@@ -314,6 +351,9 @@ class AppSync extends Sync {
     return result.data;
   }
 
+  /**
+   * Get graph client base on token from cognito
+   */
   Future<void> _getGraphClient() async {
     final AuthLink authLink = AuthLink(getToken: () => user.refreshToken);
     final Link link = authLink.concat(shared._httpLink);
@@ -323,6 +363,9 @@ class AppSync extends Sync {
     );
   }
 
+  /**
+   * Get defined schema table
+   */
   Future<void> _getSchema() async {
     var query = """
       query ListSchema {
@@ -345,15 +388,19 @@ class AppSync extends Sync {
     }
   }
 
-  List _getFields(String table) {
+  /**
+   * List available fields from schema for graphql query
+   */
+  String _getFields(String table) {
     if (schema == null || !schema.containsKey(table)) {
-      return List();
+      return '_ts\n id';
     }
 
     var schemaData = schema[table];
     // generate field types
     Map types = json.decode(schemaData['types']);
-    var fields = types.entries.map((e) => e.key).toList();
+    var fields = types.entries.map((e) => e.key).toList().join('\n');
+    fields += '\n _ts\n id';
     return fields;
   }
 }
