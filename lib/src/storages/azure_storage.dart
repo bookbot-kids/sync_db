a5f2fad648e7ef5ca1e156fb2ba7a193664bdf25
duc <duc@appiphany.com.au>
2020-12-15 00:07:56 +0700

    Do not download not exisiting file

--- lib/src/storages/azure_storage.dart
+++ lib/src/storages/azure_storage.dart
@@ -17,9 +17,14 @@ class AzureStorage extends Storage {
     try {
       var localFile = File(transferMap.localPath);
       var response = await _client.getBlob(transferMap.remotePath);
-      var ios = localFile.openWrite(mode: FileMode.write);
-      ios.add(await response.stream.toBytes());
-      await ios.close();
+      if (response.statusCode == 200) {
+        var ios = localFile.openWrite(mode: FileMode.write);
+        ios.add(await response.stream.toBytes());
+        await ios.close();
+      } else {
+        Sync.shared.logger?.e(
+            'Download [${transferMap.remotePath}] error with status code ${response.statusCode}');
+      }
     } catch (e, stackTrace) {
       Sync.shared.logger?.e('Azure Storage download error $e', e, stackTrace);
       rethrow;
