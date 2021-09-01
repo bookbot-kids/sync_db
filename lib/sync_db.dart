6f08c7a05942fc16d54f17d3ed03822ce841f64b
Hien Nguyen <hien.nguyen@mingle.com>
2021-09-01 18:06:15 +0700

    missing export s3_storage file

--- lib/sync_db.dart
+++ lib/sync_db.dart
@@ -14,4 +14,5 @@ export 'src/authenticate/cognito_user.dart';
 export 'src/model.dart';
 export 'src/storages/storage.dart';
 export 'src/storages/azure_storage.dart';
+export 'src/storages/s3_storage.dart';
 export 'src/generator/model_annotation.dart';
