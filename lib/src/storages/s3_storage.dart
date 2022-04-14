5cabb90edc6af4afe25c4123bb3f24e70590c627
duc <duc@appiphany.com.au>
2022-04-14 10:56:04 +0700

    Support stream upload into s3

--- lib/src/storages/s3_storage.dart
+++ lib/src/storages/s3_storage.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:mime_type/mime_type.dart';
-import 'package:robust_http/robust_http.dart';
+import 'package:robust_http/connection_helper.dart';
+import 'package:robust_http/exceptions.dart';
 import 'package:sync_db/sync_db.dart';
+import 'package:path/path.dart' as p;
 import 'package:universal_io/io.dart';
 
 class S3StorageUntrustedClient extends Storage {
-  HTTP http;
+  HttpClient httpClient;
+  String _baseUrl;
 
   S3StorageUntrustedClient(Map config) : super(config) {
-    http = HTTP(config['s3UploadRemoteUrl'], config);
+    _baseUrl = config['s3UploadRemoteUrl'];
+    httpClient = HttpClient()
+      ..connectionTimeout = Duration(seconds: config['connectTimeout'] ?? 60000)
+      ..idleTimeout = Duration(seconds: config['receiveTimeout'] ?? 60000);
   }
 
   @override
@@ -25,11 +33,47 @@ class S3StorageUntrustedClient extends Storage {
     var localFile = File(localPath);
     if (localFile.existsSync()) {
       final mimeType = mime(localPath) ?? '*/*';
-      http.headers = {
-        HttpHeaders.contentLengthHeader: localFile.lengthSync(),
-        HttpHeaders.contentTypeHeader: mimeType
-      };
-      await http.put('/$remotePath', data: localFile.openRead());
+      final totalByteLength = localFile.lengthSync();
+      final url =
+          _baseUrl + (remotePath.startsWith('/') ? remotePath : '/$remotePath');
+      final request = await httpClient.putUrl(Uri.parse(url));
+      request.headers.set(HttpHeaders.contentTypeHeader, mimeType);
+      request.headers.set(HttpHeaders.contentLengthHeader, totalByteLength);
+      request.headers.add('filename', p.basename(localPath));
+      request.contentLength = totalByteLength;
+
+      final fileStream = localFile.openRead();
+      final streamUpload = fileStream.transform<List<int>>(
+        StreamTransformer.fromHandlers(
+          handleData: (data, sink) {
+            sink.add(data);
+          },
+          handleError: (error, stack, sink) {
+            print(error.toString());
+          },
+          handleDone: (sink) {
+            sink.close();
+          },
+        ),
+      );
+
+      await request.addStream(streamUpload);
+      final httpResponse = await request.close();
+      if (httpResponse.statusCode != 200) {
+        if (!await ConnectionHelper.hasConnection()) {
+          throw ConnectivityException('The connection is turn off',
+              hasConnectionStatus: false);
+        }
+
+        if (!await ConnectionHelper.hasInternetConnection()) {
+          throw ConnectivityException(
+              'The connection is turn on but there is no internet connection',
+              hasConnectionStatus: true);
+        }
+
+        throw Exception(
+            'Error [${httpResponse.statusCode}] uploading file $remotePath, $httpResponse');
+      }
     }
   }
 }
