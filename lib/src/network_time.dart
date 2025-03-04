087305d9edde08127ae38974c4314937996b56bd
duc <duc@appiphany.com.au>
2025-03-04 12:57:24 +0700

    Update network timeout error handler

--- lib/src/network_time.dart
+++ lib/src/network_time.dart
@@ -3,7 +3,6 @@ import 'package:ntp/ntp.dart';
 import 'package:singleton/singleton.dart';
 import 'package:sync_db/sync_db.dart';
 import 'package:universal_io/io.dart';
-import 'package:collection/collection.dart';
 
 class NetworkTime {
   factory NetworkTime() =>
@@ -20,35 +19,35 @@ class NetworkTime {
   ];
 
   /// Get server datetime and cache the offset
-  Future<int?> get offset async {
+  Future<int?> offset(
+      {Duration timeout = const Duration(seconds: 5),
+      Function(dynamic, StackTrace)? errorCallback}) async {
     if (kIsWeb) {
       // not support for web yet
       return 0;
     }
 
     if (_offset == null) {
-      final futures = <Future<int?>>[];
-      for (final address in lookupServers) {
-        futures.add(Future.sync(() async {
-          try {
-            return await NTP
-                .getNtpOffset(
-                  localTime: DateTime.now().toLocal(),
-                  lookUpAddress: address,
-                )
-                .timeout(const Duration(seconds: 5));
-          } catch (e, stacktrace) {
-            Sync.shared.logger?.e('Can not get server time [${address}] $e',
-                error: e, stackTrace: stacktrace);
-            // Sync.shared.exceptionNotifier.value = Tuple3(false, e, stacktrace);
-          }
+      try {
+        final futures = lookupServers.map((address) => Future.sync(() async {
+              try {
+                return await NTP
+                    .getNtpOffset(
+                      localTime: DateTime.now().toLocal(),
+                      lookUpAddress: address,
+                    )
+                    .timeout(timeout);
+              } catch (e) {
+                rethrow;
+              }
+            }));
 
-          return null;
-        }));
+        _offset = await Future.any(futures);
+      } catch (e, stacktrace) {
+        _offset = 0;
+        errorCallback?.call(e, stacktrace);
       }
 
-      final results = await Future.wait(futures);
-      _offset = results.nonNulls.firstOrNull ?? 0;
       Sync.shared.logger?.i('Get ntp offset $_offset');
     }
 
@@ -62,7 +61,7 @@ class NetworkTime {
       return DateTime.now();
     }
 
-    final offsetValue = await offset;
+    final offsetValue = await offset();
     if (offsetValue == null || offsetValue == 0) {
       return DateTime.now();
     }
